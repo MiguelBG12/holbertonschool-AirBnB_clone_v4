45846e82942a0fcc28bec98f1da7cbc5cba60b6c
MiguelBG12 <miguel.bg12@gmail.com>
2024-03-02 19:39:59 -0500

    Loop through the places in the response and create article tags

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -26,6 +26,33 @@ $(document).ready(() => {
     if (data.status === 'OK') {
       // If status is 'OK', add the 'available' class to the div#api_status
       $('div#api_status').addClass('available');
+
+      // Loop through the places in the response and create article tags
+      data.places.forEach((place) => {
+        // Remove the "Owner" tag from the description
+        const description = place.description.replace('<b>Owner:</b> ', '');
+
+        // Create the article element
+        const article = `
+          <article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+            </div>
+            <div class="description">
+              ${description}
+            </div>
+          </article>
+          `;
+
+        // Append the article to the places section
+        $('.places').append(article);
+      });
     } else {
       // Otherwise, remove the 'available' class from the div#api_status
       $('div#api_status').removeClass('available');
